855d88527cdcf06832113f48bee814b2ec15742c
viktoria-karn <66881490+viktoria-karn@users.noreply.github.com>
2021-01-02 17:05:51 +0700

    Добавление функции редактирования заметки.

--- Note/Note/Note/Edit_note.Designer.cs
+++ Note/Note/Note/Edit_note.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_title = new System.Windows.Forms.TextBox();
+            this.textBox_date = new System.Windows.Forms.TextBox();
+            this.textBox_text = new System.Windows.Forms.TextBox();
+            this.button_save_edit_note = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_title
+            // 
+            this.textBox_title.Location = new System.Drawing.Point(48, 69);
+            this.textBox_title.Name = "textBox_title";
+            this.textBox_title.Size = new System.Drawing.Size(129, 20);
+            this.textBox_title.TabIndex = 0;
+            // 
+            // textBox_date
+            // 
+            this.textBox_date.Location = new System.Drawing.Point(53, 135);
+            this.textBox_date.Name = "textBox_date";
+            this.textBox_date.Size = new System.Drawing.Size(124, 20);
+            this.textBox_date.TabIndex = 1;
+            // 
+            // textBox_text
+            // 
+            this.textBox_text.Location = new System.Drawing.Point(62, 223);
+            this.textBox_text.Name = "textBox_text";
+            this.textBox_text.Size = new System.Drawing.Size(115, 20);
+            this.textBox_text.TabIndex = 2;
+            // 
+            // button_save_edit_note
+            // 
+            this.button_save_edit_note.Location = new System.Drawing.Point(479, 74);
+            this.button_save_edit_note.Name = "button_save_edit_note";
+            this.button_save_edit_note.Size = new System.Drawing.Size(111, 48);
+            this.button_save_edit_note.TabIndex = 3;
+            this.button_save_edit_note.Text = "Сохранить заметку";
+            this.button_save_edit_note.UseVisualStyleBackColor = true;
+            this.button_save_edit_note.Click += new System.EventHandler(this.button_save_edit_note_Click);
+            // 
+            // Edit_note
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_save_edit_note);
+            this.Controls.Add(this.textBox_text);
+            this.Controls.Add(this.textBox_date);
+            this.Controls.Add(this.textBox_title);
+            this.Name = "Edit_note";
             this.Text = "Edit_note";
+            this.Load += new System.EventHandler(this.Edit_note_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_title;
+        private System.Windows.Forms.TextBox textBox_date;
+        private System.Windows.Forms.TextBox textBox_text;
+        private System.Windows.Forms.Button button_save_edit_note;
     }
 }
--- Note/Note/Note/Edit_note.cs
+++ Note/Note/Note/Edit_note.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Note
 {
@@ -15,6 +16,86 @@ namespace Note
         public Edit_note()
         {
             InitializeComponent();
+        }
+
+        private void Edit_note_Load(object sender, EventArgs e)
+        {
+            textBox_title.Font = new Font("Georgia", this.Height / 25);
+            textBox_date.Font = new Font("Georgia", this.Height / 25);
+            textBox_text.Font = new Font("Georgia", this.Height / 25);
+
+
+            StreamReader fi = new StreamReader("note.txt", true);
+            string line;
+            int in_number = 0;
+
+            for (in_number = 0; in_number <= Form_main.number; ++in_number)
+            {
+                line = fi.ReadLine();
+                textBox_title.Text = line.ToString() + System.Environment.NewLine;
+                line = fi.ReadLine();
+                textBox_date.Text = line.ToString() + System.Environment.NewLine;
+                while ((line = fi.ReadLine()) != "---###---")
+                {
+                    if (in_number == Form_main.number)
+                        textBox_text.Text += line.ToString() + System.Environment.NewLine;
+                }
+            }
+            fi.Close();
+        }
+
+        private void button_save_edit_note_Click(object sender, EventArgs e)
+        {
+            int count = Form_main.number;
+            StreamReader file = new StreamReader("note.txt");
+            StreamWriter new_file = new StreamWriter("new_note.txt", false);
+            string line;
+            for (int i = 0; i < count; ++i)
+            {
+                line = file.ReadLine();
+                new_file.WriteLine(line);
+                line = file.ReadLine();
+                new_file.WriteLine(line);
+                while (line != "---###---")
+                {
+                    line = file.ReadLine();
+                    new_file.WriteLine(line);
+                }
+            }
+
+            new_file.WriteLine((textBox_title.Text).Replace(Environment.NewLine, " "));
+            new_file.WriteLine((textBox_date.Text).Replace(Environment.NewLine, " "));
+            new_file.WriteLine((textBox_text.Text).Replace(Environment.NewLine, " "));
+            new_file.WriteLine("---###---");
+
+            line = file.ReadLine();
+            line = file.ReadLine();
+            while (line != "---###---")
+            {
+                line = file.ReadLine();
+            }
+            while (!file.EndOfStream)
+            {
+                line = file.ReadLine();
+                new_file.WriteLine(line);
+            }
+            new_file.Close();
+
+            file.Close();
+            StreamWriter file_edit = new StreamWriter("note.txt", false);
+            StreamReader new_file_edit = new StreamReader("new_note.txt");
+            while (!new_file_edit.EndOfStream)
+            {
+                line = new_file_edit.ReadLine();
+                file_edit.WriteLine(line);
+
+            }
+            file_edit.Close();
+            new_file_edit.Close();
+            File.Delete("new_note.txt");
+            this.Hide();
+            Form_main form = new Form_main();
+            form.Show();
         }
     }
 }
